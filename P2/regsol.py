8f5216514a98fa43ea67d3ea870220f65b149091
Ktano <Pedrocaetano56@gmail.com>
2017-12-06 20:07:01 +0000

    passes both tests

--- P2/regsol.py
+++ P2/regsol.py
@@ -5,12 +5,15 @@ from sklearn.model_selection import cross_val_score
 import timeit
 
 def mytraining(X,Y):
-    
-   
+
+    reg= KernelRidge(kernel='rbf', gamma=0.1,alpha=0.001)
+    #reg=tree.DecisionTreeRegressor(min_samples_split=2)
+    reg.fit(X,Y)
     return reg
     
 def mytrainingaux(X,Y,par):
-    
+
+    reg= KernelRidge(kernel='rbf', gamma=par,alpha=0.1)
     reg.fit(X,Y)
                 
     return reg
@@ -20,3 +23,4 @@ def myprediction(X,reg):
     Ypred = reg.predict(X)
 
     return Ypred
+
